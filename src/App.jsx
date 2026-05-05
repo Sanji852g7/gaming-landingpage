16bc0e363612cf75aedd5b9b2a21991fe604c05a
Sanji Gurung <sanji.gurung@hotmail.com>
2026-05-05 23:12:17 +0100

    disable react-hooks/set-state-in-effect ESLint rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Hero from "./components/Hero.jsx"
-import About from "./components/About.jsx"
-import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Navbar from "./components/Navbar"
 
 const App = () => {   
   return (
